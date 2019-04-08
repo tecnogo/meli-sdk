e2bcdb733abcbe994754eebf55572a762ee26cfd
Valentin Starck <valentin.starck@gmail.com>
2019-04-07 23:29:36 -0300

    Removido metodo sin uso

--- src/Entity/LoggedUser/Notification.php
+++ src/Entity/LoggedUser/Notification.php
@@ -2,21 +2,15 @@
 
 namespace Tecnogo\MeliSdk\Entity\LoggedUser;
 
-
 use Tecnogo\MeliSdk\Entity\AbstractEntity;
 
 /**
  * Class Notification
+ *
  * @package Tecnogo\MeliSdk\LoggedUser
+ *
  * @internal
  */
 final class Notification extends AbstractEntity
 {
-    /**
-     * @return $this
-     */
-    public function save()
-    {
-        // TODO: Implement save() method.
-    }
 }

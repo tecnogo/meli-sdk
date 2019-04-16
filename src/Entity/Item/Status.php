0890b455c22501b4b261ed9c9b6e516f5ab4afff
Valentin Starck <valentin.starck@gmail.com>
2019-04-16 01:26:49 -0300

    Agregada clase tipo enum para estados de publicación

---
+++ src/Entity/Item/Status.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tecnogo\MeliSdk\Entity\Item;
+
+/**
+ * Class Status
+ *
+ * @package Tecnogo\MeliSdk\Entity\Item
+ */
+final class Status
+{
+    const ACTIVE = 'active';
+    const PAUSED = 'paused';
+    const PENDING = 'pending';
+    const PROGRAMMED = 'programmed';
+    const CLOSED = 'closed';
+    const NOT_YET_ACTIVE = 'not_yet_active';
+}

8c46ae21dc76e45393868d237f37a025f792046d
Valentin Starck <valentin.starck@gmail.com>
2019-04-23 05:29:17 -0300

    Actualizado PHPDoc

--- src/Entity/Item/Item.php
+++ src/Entity/Item/Item.php
@@ -120,12 +120,6 @@ final class Item extends AbstractEntity
      * @throws \Psr\SimpleCache\InvalidArgumentException
      * @throws \Tecnogo\MeliSdk\Exception\ContainerException
      * @throws \Tecnogo\MeliSdk\Exception\MissingConfigurationException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\BadRequestException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\ForbiddenResourceException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\InvalidTokenException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\NotFoundException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\UnexpectedHttpResponseCodeException
-     * @throws \Tecnogo\MeliSdk\Request\Exception\UnknownHttpMethodException
      * @throws \Tecnogo\MeliSdk\Request\Exception\RequestException
      */
     public function category()
